697da84641cdcd63ddbdbdd19627f6301f896692
gregory <gregory.doggen@gmail.com>
2019-04-13 10:43:39 +0200

    greg

---
+++ header.php
@@ -0,0 +1,27 @@
+<?php
+session_start();
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>PHP SQL Login Page test</title>
+    <link rel="stylesheet" href="css/main.css">
+    <script src="jquery-3.4.0.js"></script>
+</head>
+<body>
+<header>
+    <nav>
+        <a href="index.php">
+            <img src="" alt="BeCode opendeur">
+        </a>
+        <ul id="headerLinks" >
+            <li>Home</li>
+            <li>HTML</li>
+            <li>CSS</li>
+            <li>JavaScript</li>
+        </ul>
+        <div>
+        </div>
+    </nav>
+</header>
---
+++ index.php
@@ -0,0 +1,36 @@
+<?php
+require "header.php";
+?>
+<main class="vierkant">
+    <div class="left">
+
+        <div class="input">
+
+        </div>
+
+        <div class="readme">
+        <!-- text that tells you what to do -->
+        <?php require "readme.php"?>
+
+
+
+
+        </div>
+    </div>
+
+    <div class="right">
+        <h1>Welkom to becode</h1>
+        <h2>on a secion learn to code</h2>
+        <img id="imgBeCode" >
+        <p>Ullamco elit officia cupidatat excepteur irure amet consectetur minim laborum sunt.</p>
+        <p>Ullamco elit officia cupidatat excepteur irure amet consectetur minim laborum sunt.</p>
+        <p>Ullamco elit officia cupidatat excepteur irure amet consectetur minim laborum sunt.</p>
+        <p>Ullamco elit officia cupidatat excepteur irure amet consectetur minim laborum sunt.</p>
+
+    </div>
+</main>
+
+<?php
+require "footer.php";
+?>
+
